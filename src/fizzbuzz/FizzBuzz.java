a7c9b9535ef963f1dfec47e77537d36718bcfe11
tinhinane <tinhinane.ait-messaoud@univ-tlse3.fr>
2023-04-20 17:09:33 +0200

    fizz buzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,19 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+	private static final int[] numSpeciaux = {3,5};
+	private static final String[] traduction= {"FIZZ","BUZZ"};
+	
+	public String fizzbuzz (int numero) {
+		StringBuilder sb=new StringBuilder();
+		for (int i=0;i<numSpeciaux.length;i++) {
+			if (numero%numSpeciaux[i]==0)
+				sb.append( traduction[i]);
+		}
+		
+		if(sb.length()==0)
+			return Integer.toString(numero);
+		return sb.toString();
+	}
 
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -6,14 +6,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
+	private FizzBuzz fb;
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
+		fb=new FizzBuzz();
 	}
 
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void fizzbuzz_repond_1_pour_1() {
+		assertEquals("1",fb.fizzbuzz(1));
+	}
+	
+	@Test
+	void fizzbuzz_repond_2_pour_2() {
+		assertEquals("2",fb.fizzbuzz(2));
+	}
+	
+	@Test
+	void fizzbuzz_repond_un_entier() {
+		assertEquals("4",fb.fizzbuzz(4));
+		assertEquals("7",fb.fizzbuzz(7));
+		assertEquals("13",fb.fizzbuzz(13));
+		assertEquals("BUZZ",fb.fizzbuzz(400));
+	}
+	@Test
+	void fizzbuzz_repond_3_pou_3() {
+		assertEquals("FIZZ",fb.fizzbuzz(3));
+	}
+	
+	@Test 
+	void fizzbuzz_repond_fizz() {
+		assertEquals("FIZZ",fb.fizzbuzz(3));
+	}
+	
+	@Test
+	void fizzbuzz_repond_buzz() {
+		assertEquals("BUZZ",fb.fizzbuzz(5));
+	}
+	
+	@Test
+	void fizzbuzz_repond_fizzbuzz() {
+		 assertEquals("FIZZBUZZ",fb.fizzbuzz(15));
 	}
 
 }
